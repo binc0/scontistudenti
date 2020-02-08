ab3bae6292df77af16e0468f9b20345a7f0e88df
binc0 <42704408+binc0@users.noreply.github.com>
2020-02-08 01:57:21 +0100

    up

---
+++ js/offerte.js
@@ -0,0 +1,41 @@
+ var settings = {
+  "async": true,
+  "url": "https://raw.githubusercontent.com/binc0/studenti/master/csvjson.json,
+  "method": "GET",
+};
+
+$.ajax(settings).done(function (jcontent){
+    
+    console.log(jcontent.Nome);
+   
+for (var i = 0; i < jcontent.count; i++) {
+    console.log(i);
+
+var output = document.getElementById('offerte');
+output.innerHTML += '';
+    
+
+
+}
+
+});
+
+
+
+
+<div class="col-md-6 mb-4 mb-lg-4 col-lg-4">
+            
+            <div class="listing-item">
+              <div class="listing-image">
+                <img src="images/img_1.jpg" alt="Free Website Template by Free-Template.co" class="img-fluid">
+              </div>
+              <div class="listing-item-content">
+                <a href="listings-single.html" class="bookmark" data-toggle="tooltip" data-placement="left" title="Bookmark"><span class="icon-heart"></span></a>
+                <a class="px-3 mb-3 category" href="#">Hotels</a>
+                <h2 class="mb-1"><a href="listings-single.html">Luxe Hotel</a></h2>
+                <span class="address">West Orange, New York</span>
+              </div>
+            </div>
+
+          </div>
+
