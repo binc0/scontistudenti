6fc5302645cf6bf98de7daa0e20f1a2b9218015e
binc0 <42704408+binc0@users.noreply.github.com>
2020-02-08 02:07:50 +0100

    Update offerte.js

--- js/offerte.js
+++ js/offerte.js
@@ -6,7 +6,7 @@
 
 $.ajax(settings).done(function (jcontent){
     
-    console.log(jcontent.Nome(0));
+    console.log(jcontent(0).Nome);
    
 for (var i = 0; i < jcontent.count; i++) {
     console.log(i);
