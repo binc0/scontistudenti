8efa9b79eb2b2018ca0b8632faa6d2e8b06a5f67
binc0 <42704408+binc0@users.noreply.github.com>
2020-02-08 02:00:25 +0100

    up

--- js/offerte.js
+++ js/offerte.js
@@ -23,19 +23,5 @@ output.innerHTML += '';
 
 
 
-<div class="col-md-6 mb-4 mb-lg-4 col-lg-4">
-            
-            <div class="listing-item">
-              <div class="listing-image">
-                <img src="images/img_1.jpg" alt="Free Website Template by Free-Template.co" class="img-fluid">
-              </div>
-              <div class="listing-item-content">
-                <a href="listings-single.html" class="bookmark" data-toggle="tooltip" data-placement="left" title="Bookmark"><span class="icon-heart"></span></a>
-                <a class="px-3 mb-3 category" href="#">Hotels</a>
-                <h2 class="mb-1"><a href="listings-single.html">Luxe Hotel</a></h2>
-                <span class="address">West Orange, New York</span>
-              </div>
-            </div>
 
-          </div>
 
